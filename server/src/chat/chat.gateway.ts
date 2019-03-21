fdf0f449a6f8357293a152517d8020c3c52baa32
CirnoBreak <1103054869@qq.com>
2019-03-21 22:37:38 +0800

    server: init chat gateway(socketio)

---
+++ server/src/chat/chat.gateway.ts
@@ -0,0 +1,18 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse
+} from '@nestjs/websockets';
+import { Client, Server } from 'socket.io';
+
+@WebSocketGateway()
+export class ChatGateway {
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('sendMsg')
+  sendMsg(client: Client, data: any) {
+    console.log(client, data);
+  }
+}
--- server/src/chat/chat.module.ts
+++ server/src/chat/chat.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ChatSchema } from './schema/chat.schema';
+import { ChatGateway } from './chat.gateway';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Chat', schema: ChatSchema }])]
+  imports: [MongooseModule.forFeature([{ name: 'Chat', schema: ChatSchema }])],
+  providers: [ChatGateway]
 })
 export class ChatModule {}
